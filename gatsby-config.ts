0591c58eda77729cc725ad6e88c013264cea072a
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2024-01-28 23:04:29 +0100

    init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,85 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Wedding`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
-};
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: process.env.GOOGLE_TAG_MANAGER_TRACKING_ID,
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: 'gatsby' },
 
-export default config;
+        // Specify optional GTM environment details.
+        gtmAuth: process.env.GOOGLE_TAG_MANAGER_AUTH,
+        gtmPreview: process.env.GOOGLE_TAG_MANAGER_PREVIEW,
+
+        // Defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
+      // this is the default configuration, override only what you need
+      options: {
+        cwd: process.cwd(), // path to a directory that should be considered as the current working directory
+        watch: true, // format/lint on save
+        initialScan: true, // if true, will format/lint the whole project on Gatsby startup
+        onChangeFullScanLint: false, // if true, on file save always perform full scan lint
+        onChangeFullScanFormat: false, // if true, on file save always perform full scan format
+        prettierLast: false, // if true, will run Prettier after ESLint
+        ignorePatterns: [
+          '**/node_modules/**/*',
+          '**/.git/**/*',
+          '**/dist/**/*',
+          '.cache/**/*',
+          'public/**/*',
+        ], // string or array of paths/files/globs to ignore
+        prettier: {
+          patterns: [], // string or array of paths/files/globs to include related only to Prettier
+          ignorePatterns: [], // string or array of paths/files/globs to exclude related only to Prettier
+          customOptions: {}, // see: https://prettier.io/docs/en/options.html
+        },
+        eslint: {
+          patterns: [], // string or array of paths/files/globs to include related only to ESLint
+          ignorePatterns: [], // string or array of paths/files/globs to exclude related only to ESLint
+          formatter: 'stylish', // set custom or third party formatter
+          maxWarnings: undefined, // number of max warnings allowed, when exceed it will fail Gatsby build
+          emitWarning: true, // if true, will emit lint warnings
+          failOnError: false, // if true, any lint error will fail the build, you may set true only in your prod config
+          failOnWarning: false, // same as failOnError but for warnings
+          plugins: [], // an array of plugins to load for ESLint
+          customOptions: {}, // see: https://eslint.org/docs/developer-guide/nodejs-api#cliengine
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}
+
+export default config
