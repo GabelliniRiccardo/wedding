35d77fc7832c0b9478370fc40a26899f3456f63e
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2024-01-29 23:41:42 +0100

    Added welcome.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 // Layout.tsx
 import React, { ReactNode } from 'react'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 import { createTheme, ThemeProvider } from '@mui/material'
@@ -24,20 +24,22 @@ const Layout: React.FC<{ children: ReactNode; showNavbar: boolean }> = ({
 }) => {
   return (
     <ThemeProvider theme={customTheme}>
-      <motion.div
-        key="layout"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
-        className="h-full"
-      >
-        {showNavbar && <Navbar />}
-        <main className={`min-h-full${showNavbar ? ' mt-20' : ''}`}>
-          {children}
-        </main>
-        <Footer />
-      </motion.div>
+      <AnimatePresence>
+        <motion.div
+          key="layout"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 2, ease: 'easeInOut' }}
+          className="h-full"
+        >
+          {showNavbar && <Navbar />}
+          <main className={`min-h-full${showNavbar ? ' mt-20' : ''}`}>
+            {children}
+          </main>
+          <Footer />
+        </motion.div>
+      </AnimatePresence>
     </ThemeProvider>
   )
 }
