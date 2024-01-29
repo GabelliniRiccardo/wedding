46c84ebbf849b97b793df1bcf0bd5da8eda30311
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2024-01-29 22:39:24 +0100

    Added welcome.tsx

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,11 +1,12 @@
 // src/components/Welcome.js
 import React, { useEffect, useState } from 'react'
 import { Container, CssBaseline, Typography } from '@mui/material'
-import { useLocation } from '@reach/router'
+import { useLocation } from '@reach/router' // Import useLocation from @reach/router
 import Layout from '../components/Layout/Layout'
+import { TypeAnimation } from 'react-type-animation'
 
 const Welcome = () => {
-  const location = useLocation()
+  const location = useLocation() // Use useLocation from @reach/router
   const [usernames, setUsernames] = useState<string[]>([])
 
   useEffect(() => {
@@ -20,19 +21,39 @@ const Welcome = () => {
 
   const generateWelcomeMessage = () => {
     if (usernames.length === 0) {
-      return 'No usernames found in the query parameters.'
+      console.error('usernames not found!')
+      return 'Hello'
     }
 
-    return `Hello ${usernames.join(', ')}, welcome to our wedding!`
+    return `Hello ${usernames.join(', ')},`
   }
 
   return (
     <Layout>
-      <div className="h-screen flex items-center justify-center">
-        <Typography component="h1" variant="h5">
-          {generateWelcomeMessage()}
-        </Typography>
-      </div>
+      <Container>
+        <div className="h-screen flex items-center justify-center">
+          <Typography
+            component="h1"
+            variant="h5"
+            style={{ fontFamily: 'YourDesiredFont' }}
+          >
+            {usernames.length && (
+              <TypeAnimation
+                sequence={[
+                  generateWelcomeMessage(),
+                  1000,
+                  'Welcome to our wedding!',
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                style={{ fontSize: '2em', display: 'inline-block' }}
+                repeat={Infinity}
+              />
+            )}
+          </Typography>
+        </div>
+      </Container>
     </Layout>
   )
 }
