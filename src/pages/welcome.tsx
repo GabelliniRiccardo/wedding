dc91163aaaeadeb20ed4ede928957519f174621b
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2024-01-30 00:40:29 +0100

    Added welcome.tsx

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -2,11 +2,13 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Typography } from '@mui/material'
 import { useLocation } from '@reach/router'
-import { TypeAnimation } from 'react-type-animation'
+import { motion } from 'framer-motion'
 import Layout from '../components/Layout/Layout'
 import { navigate } from 'gatsby'
+import { TypeAnimation } from 'react-type-animation'
 
 const writingSpeed = 20
+
 const Welcome = () => {
   const location = useLocation()
   const [usernames, setUsernames] = useState<string[]>([])
@@ -20,19 +22,6 @@ const Welcome = () => {
       setUsernames(parsedUsernames)
     }
   }, [location.search])
-
-  useEffect(() => {
-    if (usernames.length > 0) {
-      const animationDuration =
-        generateWelcomeMessage().length * writingSpeed * 7 // Assuming each character takes 20ms to animate and 7 seconds to be written
-
-      const timeoutId = setTimeout(async () => {
-        await navigate('/')
-      }, animationDuration)
-
-      return () => clearTimeout(timeoutId)
-    }
-  }, [usernames.length])
 
   const generateWelcomeMessage = () => {
     if (usernames.length === 0) {
@@ -49,29 +38,73 @@ const Welcome = () => {
     return `Ciao ${penultimateNames} e ${lastUsername},\nBenvenuti!`
   }
 
+  const variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  }
+
+  const handleScroll = () => {
+    const windowHeight = window.innerHeight
+    const documentHeight = document.documentElement.scrollHeight
+    const currentScroll = window.scrollY
+
+    const isScrolledToBottom = currentScroll + windowHeight >= documentHeight
+
+    if (isScrolledToBottom && usernames.length > 0) {
+      navigate('/')
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [usernames.length])
+
   return (
     <Layout showNavbar={false}>
       {usernames.length && (
-        <div className="h-screen flex mt-40 sm:mt-0 items-start sm:items-center justify-center text-center">
-          <Typography
-            component="h1"
-            variant="h1"
-            style={{ fontFamily: 'LovedbytheKingRegular' }}
+        <div className="h-full overflow-y-scroll flex flex-col mt-40 sm:mt-0 items-start sm:items-center justify-center text-center">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            transition={{ ease: 'easeOut', duration: 0.8 }}
+            className="flex flex-col justify-center h-screen"
           >
-            <div className="flex flex-col justify-end">
-              <TypeAnimation
-                style={{
-                  whiteSpace: 'pre-line',
-                  display: 'block',
-                }}
-                sequence={[generateWelcomeMessage()]}
-                wrapper="div"
-                speed={writingSpeed}
-                repeat={0}
-                cursor={false}
-              />
-            </div>
-          </Typography>
+            <TypeAnimation
+              style={{
+                whiteSpace: 'pre-line',
+                fontFamily: 'LovedbytheKingRegular',
+              }}
+              className="text-7xl"
+              sequence={[generateWelcomeMessage()]}
+              wrapper="div"
+              speed={writingSpeed}
+              repeat={0}
+              cursor={false}
+            />
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            transition={{ ease: 'easeOut', duration: 0.8 }}
+            className="flex flex-col justify-center h-screen"
+          >
+            <TypeAnimation
+              style={{
+                whiteSpace: 'pre-line',
+                fontFamily: 'LovedbytheKingRegular',
+              }}
+              className="text-7xl"
+              sequence={['Finalmente Ci Sposiamo!']}
+              wrapper="div"
+              speed={writingSpeed}
+              repeat={0}
+              cursor={false}
+            />
+          </motion.div>
         </div>
       )}
     </Layout>
