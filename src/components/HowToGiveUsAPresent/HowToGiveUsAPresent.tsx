6237222f85db26cec6533bac7c82fd0c711270b6
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2024-04-02 20:56:05 +0200

    Improved

--- src/components/HowToGiveUsAPresent/HowToGiveUsAPresent.tsx
+++ src/components/HowToGiveUsAPresent/HowToGiveUsAPresent.tsx
@@ -1,14 +1,24 @@
-import * as React from 'react'
+import React, { useState } from 'react'
+import { Tooltip } from '@mui/material'
 import gifImage from '../../images/present.gif'
 
 const HowToGiveUsAPresent = () => {
+  const iban = 'IT00 XXXX XXXX XXXX XXXX XXXX XXX'
+  const [copySuccess, setCopySuccess] = useState(false)
+
+  const copyToClipboard = () => {
+    const ibanWithoutSpaces = iban.replace(/\s/g, '')
+    navigator.clipboard.writeText(ibanWithoutSpaces)
+    setCopySuccess(true)
+    setTimeout(() => {
+      setCopySuccess(false)
+    }, 3000)
+  }
+
   return (
-    <div className="bg-w-yellow-lighter p-6 rounded-lg shadow-lg">
+    <div className="bg-yellow-100 p-6 rounded-lg shadow-lg">
       <div className="flex flex-col justify-center items-center mb-8 text-lg gap-3">
-        <h2
-          className="text-4xl font-bold mb-4 text-center"
-          id="how-to-give-us-a-present-section"
-        >
+        <h2 className="text-4xl font-bold mb-4 text-center">
           Come darci un regalo
         </h2>
         <p>
@@ -19,10 +29,22 @@ const HowToGiveUsAPresent = () => {
           Tuttavia, se desiderate fare un pensiero, ecco le coordinate bancarie
           per effettuare un bonifico:
         </p>
-        <p className="font-bold">IBAN: IT00 XXXX XXXX XXXX XXXX XXXX XXX</p>
-        <div className="flex flex-wrap justify-between">
-          <img src={gifImage} alt="GIF" className="max-w-20 ml-auto" />
+        <p>P.s. se si clicca l'IBAN viene automaticamente copiato</p>
+        <div className="flex gap-1 mt-8">
+          <p className="font-bold">IBAN:</p>
+          <Tooltip
+            title={copySuccess ? 'Copiato!' : 'Clicca per copiare'}
+            arrow
+          >
+            <p
+              className="text-lg cursor-pointer text-blue-500"
+              onClick={copyToClipboard}
+            >
+              {iban}
+            </p>
+          </Tooltip>
         </div>
+        <img src={gifImage} alt="GIF" className="max-w-24" />
       </div>
     </div>
   )
