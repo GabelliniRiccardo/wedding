b2c99dfb454200d0079dca9ae19c8ac4e29b45df
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2024-04-02 19:55:00 +0200

    Testing google map key

--- src/components/GoogleMaps/GoogleMaps.tsx
+++ src/components/GoogleMaps/GoogleMaps.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react'
 import { Fragment, useEffect, useState } from 'react'
 
-const GOOGLE_MAPS_SRC = process.env.GOOGLE_MAPS_SRC
-
 const GoogleMaps = () => {
+  const [isClient, setIsClient] = useState(false)
+  const GOOGLE_MAPS_SRC = process.env.GOOGLE_MAPS_SRC
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
     <Fragment>
-      This is the google map key {GOOGLE_MAPS_SRC}
-      {GOOGLE_MAPS_SRC && (
-        <div className="rounded-lg overflow-hidden shadow-lg">
-          <iframe
-            src={GOOGLE_MAPS_SRC}
-            title="Google Maps"
-            className="w-full h-96"
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
+      {isClient && (
+        <Fragment>
+          {GOOGLE_MAPS_SRC && (
+            <div className="rounded-lg overflow-hidden shadow-lg">
+              <iframe
+                src={GOOGLE_MAPS_SRC}
+                title="Google Maps"
+                className="w-full h-96"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          )}
+        </Fragment>
       )}
     </Fragment>
   )
