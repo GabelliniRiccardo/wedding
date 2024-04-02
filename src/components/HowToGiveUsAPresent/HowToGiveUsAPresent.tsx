6a00ef572bcd8f9f5820ddcd85ee6ea92f1da854
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2024-04-02 21:01:35 +0200

    Improved

--- src/components/HowToGiveUsAPresent/HowToGiveUsAPresent.tsx
+++ src/components/HowToGiveUsAPresent/HowToGiveUsAPresent.tsx
@@ -23,7 +23,7 @@ const HowToGiveUsAPresent = () => {
         </h2>
         <p>
           Non è necessario un regalo, poiché ciò che conta davvero è la vostra
-          preziosa presenza.
+          <span className="font-bold"> preziosa</span> presenza.
         </p>
         <p>
           Tuttavia, se desiderate fare un pensiero, ecco le coordinate bancarie
@@ -31,18 +31,28 @@ const HowToGiveUsAPresent = () => {
         </p>
         <p>P.s. se si clicca l'IBAN viene automaticamente copiato</p>
         <div className="flex gap-1 mt-8">
-          <p className="font-bold">IBAN:</p>
-          <Tooltip
-            title={copySuccess ? 'Copiato!' : 'Clicca per copiare'}
-            arrow
-          >
-            <p
-              className="text-lg cursor-pointer text-blue-500"
-              onClick={copyToClipboard}
+          <div className="mt-4">
+            <p className="font-bold">Beneficiario:</p>
+            <p>Riccardo Gabellini</p>
+            <p className="font-bold mt-2">Indirizzo Banca:</p>
+            <p>Nome Banca</p>
+            <p className="font-bold mt-2">IBAN:</p>
+            <Tooltip
+              title={copySuccess ? 'Copiato!' : 'Clicca per copiare'}
+              arrow
             >
-              {iban}
-            </p>
-          </Tooltip>
+              <p
+                className="text-lg cursor-pointer text-blue-500"
+                onClick={copyToClipboard}
+              >
+                {iban}
+              </p>
+            </Tooltip>
+            <p className="font-bold mt-2">Codice SWIFT/BIC:</p>
+            <p>SWIFT/BIC</p>
+            <p className="font-bold mt-2">Causale:</p>
+            <p>Causale</p>
+          </div>
         </div>
         <img src={gifImage} alt="GIF" className="max-w-24" />
       </div>
